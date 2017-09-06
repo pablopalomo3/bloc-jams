6411e9583d46fa0ffa8f535a639026a351c49323
pablopalomo3 <pablopalomo3@gmail.com>
2017-09-06 08:27:27 -0500

    revision to landing

--- scripts/landing.js
+++ scripts/landing.js
@@ -26,17 +26,20 @@
 //     revealSecondPoint();
 //     revealThirdPoint();
 // };
-
-var revealPoint = function() {
+var animatePoints = function() {
     var points = document.getElementsByClassName('point');
-    for(var i = 0; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
         // playing with different transitions
-        points[i].style.transitionDuration = "1s";
-        points[i].style.border = "2px dotted pink";
+        //points[index].style.transitionDuration = "1s";
+        //points[index].style.border = "2px dotted pink";
+    }
+    for (var i = 0; i < points.length; i++){
+      revealPoint(i);
     }
 }
-//revealPoint();
+//animatePoints();
