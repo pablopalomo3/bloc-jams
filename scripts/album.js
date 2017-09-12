cf9c5acb38e418c8077e2c5a9cb750e6fc9ec01d
Pablo Palomo III <pablopalomo3@gmail.com>
2017-09-11 21:58:04 -0500

    assignment 11 completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@
      ]
  };
 
+ // Assignment Album
+ var albumZombie = {
+     title: 'The Zombie',
+     artist: 'Dead Walker',
+     label: 'BFG',
+     year: '2020',
+     albumArtUrl: 'assets/images/album_covers/07.png',
+     songs: [
+         { title: 'Apocalypse', duration: '1:01' },
+         { title: 'Run, Run, Run', duration: '5:01' },
+         { title: 'Brains are good', duration: '3:21'},
+         { title: 'Rick Grimes', duration: '3:14' },
+         { title: 'Zombie', duration: '2:15'}
+     ]
+ };
+
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -42,14 +58,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 }
 
-var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+var setCurrentAlbum = function(album) {
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -67,4 +82,15 @@ var setCurrentAlbum = function(album) {
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumZombie];
+     var index = 1;
+
+     albumImage.addEventListener("click", function(event) {
+         setCurrentAlbum(albums[index]);
+         index++;
+         if (index == albums.length) {
+             index = 0;
+         }
+     });
  };
